46c0a1d43073caf2c0456fadc6378fcc0323e641
xkl <joselucas20048jl@gmail.com>
2023-11-24 12:57:12 -0300

    Initial commit

---
+++ src/context/CurrencyContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+export const CurrencyContext = createContext();
+
+export const CurrencyProvider = ({ children }) => {
+  const [fromCurrency, setFromCurrency] = useState("🇺🇸 USD - United States");
+  const [toCurrency, setToCurrency] = useState("🇦🇺 AUD - Australia");
+  const [firstAmount, setFirstAmount] = useState("");
+
+  const value = {
+    fromCurrency,
+    setFromCurrency,
+    toCurrency,
+    setToCurrency,
+    firstAmount,
+    setFirstAmount,
+  };
+  return (
+    <CurrencyContext.Provider value={value}>
+      {children}
+    </CurrencyContext.Provider>
+  );
+};
